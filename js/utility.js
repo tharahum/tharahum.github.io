d36e1c87bfb1bede96885b52ccaa8f2c1585169e
tharahum <tharahum@gmail.com>
2020-06-01 21:37:49 +0530

    first commit

---
+++ js/utility.js
@@ -0,0 +1,13 @@
+var welcomeText = "We are trying to groom a society, which encourages Sadaqah, educates under-previliged kids, helping heal poor and nurtures the next generation of ummah imbibed with virtuous qualities."+"<br/>"+"<br/>";
+
+    welcomeText =  welcomeText + "Currently, we are actively working in the area of eduction, skill development, healthcare and social awareness."+"<br/>"+"<br/>";
+	
+	    welcomeText =  welcomeText + "Scroll below to see our causes. "+"<br/>"+"<br/>";
+	               
+	
+
+function fnWelcometoOurCharity(){
+    document.getElementById("btnWelcomeToOurCharity").style.display="none";
+	document.getElementById("welcomeToOurCharity").innerHTML+=welcomeText;
+	return true;
+}
